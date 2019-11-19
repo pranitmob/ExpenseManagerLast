8c0cbe268afb22eab56d8c009a6cfc3de08c0fc2
pranitmob <pranitmob@github.com>
2019-11-19 18:21:12 +0530

    Enable CORS

--- ExpenseManager-master/src/main/java/com/app/ExpenseManagerLast/controller/UserController.java
+++ ExpenseManager-master/src/main/java/com/app/ExpenseManagerLast/controller/UserController.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +26,7 @@ import com.app.ExpenseManagerLast.service.IUserService;
 /**
  * @author Pranit
  */
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/user")
 
