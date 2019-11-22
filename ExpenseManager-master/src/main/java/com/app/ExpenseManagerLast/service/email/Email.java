b00177de795fbb39856fae70ffc9b1feb5ccb2a7
govindkapure <govindkapure98@gmail.com>
2019-11-22 15:40:05 +0530

    email with attachment

--- ExpenseManager-master/src/main/java/com/app/ExpenseManagerLast/service/email/Email.java
+++ ExpenseManager-master/src/main/java/com/app/ExpenseManagerLast/service/email/Email.java
@@ -1,8 +1,15 @@
 package com.app.ExpenseManagerLast.service.email;
 
+import java.io.File;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,5 +26,18 @@ public class Email
 		simpleMailMessage.setText("Test message for spring boot application");
 		
 		javaMailSender.send(simpleMailMessage);
+	}
+	
+	public void sendEmailWithAttachment() throws MessagingException
+	{
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+		mimeMessageHelper.setTo("pranit.sakunde@mobiliya.com");
+		mimeMessageHelper.setSubject("Test Mail for Spring boot application with attachment");
+		mimeMessageHelper.setText("Test Mail for Spring boot application with attachment");
+		
+		FileSystemResource fileSystemResource = new FileSystemResource(new File("pom.xml"));
+		mimeMessageHelper.addAttachment("Test", fileSystemResource);
+		javaMailSender.send(mimeMessage);
 	}
 }
